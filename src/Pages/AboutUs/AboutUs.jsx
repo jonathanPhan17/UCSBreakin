513b160b852597497db2841993eccf2b1671a890
jonathanPhan17 <shoxie1999@gmail.com>
2024-09-03 15:00:46 -0700

    add styles and svg logo

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,23 +1,29 @@
 import React from "react";
+import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
     <div className="about-us-container">
-      <header>
-        <h2>We are UCSBreakin</h2>
+      <header className="about-us-header">
+        <h2>WE ARE UCSBREAKIN</h2>
         <h3>"A modern family connected through dance since 2010"</h3>
       </header>
-      <main>
+      <main className="about-us-content">
         <p>
           Born and raised in the city of{" "}
           <a
             href="https://www.google.nl/maps/place/Goleta,+CA/"
             target="_blank"
+            className="about-us-content-link"
           >
             Goleta
           </a>{" "}
           in the coast of{" "}
-          <a href="https://www.google.nl/maps/place/California" target="_blank">
+          <a
+            href="https://www.google.nl/maps/place/California"
+            target="_blank"
+            className="about-us-content-link"
+          >
             California
           </a>
           . From taking the competition game a little too seriously, our
